22208d5b42c9a85aa945d95afbfa55b4170ab2ca
sarahPC <sarahapplebaum.sia@gmail.com>
2023-03-09 23:58:33 -0500

    work

---
+++ Assets/Prospector/__Scripts/CardSpritesSO.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu( fileName = "CardSprites", menuName = "ScriptableObjects/CardSpritesSO")]
+public class CardSpritesSO : ScriptableObject
+{
+    private static CardSpritesSO S;
+
+     public Sprite cardBack;
+     public Sprite cardBackGold;
+     public Sprite cardFront;
+    public Sprite cardFrontGold;
+
+
+    public Sprite suitDiamond;
+    public Sprite suitClub;
+    public Sprite suitHeart;
+    public Sprite suitSpade;
+
+    public List<Sprite> faceSprites;
+    public List<Sprite> rankSprites;
+
+    public static Dictionary<char, Sprite> SUITS { get; private set;}
+
+    public void Init(){
+        INIT_STATICS(this);
+    }
+
+    static void INIT_STATICS(CardSpritesSO cSSO){
+        if (S!=null)
+        {
+            return;
+        }
+        S = cSSO;
+
+
+        SUITS = new Dictionary<char, Sprite>{
+            {'C', S.suitClub},
+            {'D', S.suitDiamond},
+            {'H', S.suitHeart},
+            {'S', S.suitSpade}
+        };
+    }
+
+    public static List<Sprite> RANKS{
+        get{ return S.rankSprites;}
+    }
+
+    public static Sprite GET_FACE(string name){
+        foreach (var item in S.faceSprites)
+        {
+            if (item.name == name) return item;
+        }
+        return null;
+    }
+
+    public static Sprite BACK{
+        get { return S.cardBack; }
+    }
+
+    public static void RESET(){
+        S = null;
+    }
+
+}
--- Assets/Prospector/__Scripts/JsonParseDeck.cs
+++ Assets/Prospector/__Scripts/JsonParseDeck.cs
@@ -2,17 +2,51 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class JsonParseDeck : MonoBehaviour
+[System.Serializable]
+public class JsonPip
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    public string type = "pip";
+    public Vector3 loc;
+    public bool flip = false;
+    public float scale = 1;
+}
+[System.Serializable]
+public class JsonCard{
+    public int rank;
+    public string face;
+    public List<JsonPip> pips = new List<JsonPip>();
+}
+
+[System.Serializable]
+public class JsonDeck{
+    public List<JsonPip>  decorators = new List<JsonPip>();
+    public List<JsonCard> cards = new List<JsonCard>();
+}
+
+public class JsonParseDeck{
+    private static JsonParseDeck S {get; set;}
+
+    public TextAsset jsonDeckFile;
+    public JsonDeck deck;
+    private void Awake() {
+        if (S!=null)
+        {
+            return;
+        }
+        S = this;
+        deck = JsonUtility.FromJson<JsonDeck>(jsonDeckFile.text);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+
+    public static List<JsonPip> DECORATORS{
+        get{return S.deck.decorators;}
     }
+    static public JsonCard GET_CARD_DEF(int rank){
+        if ((rank < 1) || (rank > S.deck.cards.Count))
+        {
+            return null;
+        }
+        return S.deck.cards[rank - 1];
+    }
+
 }
